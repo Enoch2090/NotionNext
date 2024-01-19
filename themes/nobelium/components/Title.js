2cddeb3ac24c8b6f808adce595faed0922286b26
enoch2090 <gyc990926@gmail.com>
2024-01-19 13:23:49 +0800

    init

--- themes/nobelium/components/Title.js
+++ themes/nobelium/components/Title.js
@@ -6,14 +6,17 @@ import { siteConfig } from '@/lib/config'
  * @returns
  */
 export const Title = (props) => {
-  const { post } = props
-  const title = post?.title || siteConfig('DESCRIPTION')
-  const description = post?.description || siteConfig('AUTHOR')
+    const { post } = props
+    const title = post?.title || siteConfig('DESCRIPTION')
+    const description = post?.description || siteConfig('AUTHOR')
 
-  return <div className="text-center px-6 py-12 mb-6 bg-gray-100 dark:bg-hexo-black-gray dark:border-hexo-black-gray border-b">
-        <h1 className=" text-xl md:text-4xl pb-4">{title}</h1>
-        <p className="leading-loose text-gray-dark">
-            {description}
-        </p>
-    </div>
+    return (
+        <div className="text-center px-6 py-12 mb-6 bg-gray-100 dark:bg-hexo-black-gray dark:border-hexo-black-gray border-b">
+            <h1 className="text-xl md:text-4xl pb-4 font-bold">{title}</h1>
+            <p className="leading-loose text-gray-dark">
+                {description}
+            </p>
+        </div>
+    );
+
 }
